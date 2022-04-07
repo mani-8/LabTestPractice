de5b295e5945c5a69c8453c44ffed02b4febc097
mani-8 <98014465+mani-8@users.noreply.github.com>
2022-04-07 21:27:59 +0530

    Add files via upload

---
+++ NumberPattern.java
@@ -0,0 +1,22 @@
+package BasicProgram;
+
+public class NumberPattern {
+	public static void main(String []args) {
+		for(int i=4;i>=1;i--) {
+			 for(int j=1;j<=4;j++) {
+				 if(j<=i)
+			    	  System.out.print(j);
+			      else
+			          System.out.print("*"); 
+			 }
+			 for(int j=4;j>=1;j--) {
+				 if(j<=i)
+			    	  System.out.print(j);
+			      else
+			    	  System.out.print("*");
+			 }
+			 System.out.println();
+		}
+	}
+}
+
