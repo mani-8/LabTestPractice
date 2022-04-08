80501afc838db31cee368fffd3b863e4324c6ecd
mani-8 <98014465+mani-8@users.noreply.github.com>
2022-04-08 08:09:00 +0530

    Add files via upload

---
+++ Pattern1.java
@@ -0,0 +1,15 @@
+package BasicProgram;
+
+public class Pattern1 {
+public static void main(String []args) {
+	int k;
+	for(int i=1;i<=10;i++)
+	{	
+		for(int j=1;j<=i;j++)
+		{
+			k=j*i;
+			System.out.print(k+" ");
+		}System.out.println();
+}
+}
+}
