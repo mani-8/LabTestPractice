0e5cb3c7f4d0e5fe555bf0c038bc0717c4a02778
mani-8 <98014465+mani-8@users.noreply.github.com>
2022-04-29 13:18:24 +0530

    Add files via upload

---
+++ BinarySearch.java
@@ -0,0 +1,35 @@
+package BasicProgram;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class BinarySearch {
+	public static void main(String args[]){
+		
+        int arr[],mid,fst_index,lst_index;	
+		arr =new int[] {2,3,15,53,10,12,7}; 
+		Arrays.sort(arr);
+		for(int i=0;i<arr.length;i++) {
+			System.out.print(arr[i]+" ");
+		}
+		Scanner sc = new Scanner(System.in);
+		System.out.println("\nEnter number to find the index of thise number : ");
+		int key=sc.nextInt();
+		
+		fst_index=0; 
+		lst_index = arr.length-1;
+		
+		while( fst_index <= lst_index ){	
+			mid = (fst_index + lst_index)/2;
+			if(arr[mid]<key) {
+				fst_index = mid+1;
+			}else if(arr[mid]==key){
+				System.out.println("The number "+key+" is found at index "+mid);
+				break;
+			}else {
+				lst_index=mid-1;
+			}
+		}
+		sc.close();
+	}
+}
